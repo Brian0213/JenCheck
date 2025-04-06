9aefd55f47b1f9988cb0e3ee8cca1338a2d2c676
Brian0213 <olusegunojeyinka@gmail.com>
2025-04-06 03:08:15 -0400

    Adding Dependencies

---
+++ testScripts/conftest.py
@@ -0,0 +1,8 @@
+# import options as options
+from selenium import webdriver
+import pytest
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+# import chromedriver_autoinstaller as chromedriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
