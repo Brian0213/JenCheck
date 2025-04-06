300485853e65ba3c1d1a3d91b0c01161a952b11d
Brian0213 <olusegunojeyinka@gmail.com>
2025-04-06 10:29:35 -0400

    Added AddProducts Page and TestScripts

--- testScripts/conftest.py
+++ testScripts/conftest.py
@@ -1,7 +1,10 @@
 # import options as options
+
 from selenium import webdriver
 import pytest
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 # import chromedriver_autoinstaller as chromedriver
 from webdriver_manager.firefox import GeckoDriverManager
@@ -14,10 +17,18 @@ chrome_options = Options()
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-notifications")
+options.add_argument("--disable-popup-blocking")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-dev-shm-usage")
 # options.add_argument("headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
+options.add_experimental_option("prefs", {
+    "profile.default_content_setting_values.notifications": 2
+})
 
 @pytest.fixture()
 def setup(browser):
@@ -29,9 +40,11 @@ def setup(browser):
     return driver
 
 
-def pytest_addoption(parser):      # This will get the value from CLI/hooks
+def pytest_addoption(parser):    # This will get the value from CLI/hooks
     parser.addoption("--browser")
 
 @pytest.fixture()
 def browser(request):    # This will return the Browser value to the setup method.
     return request.config.getoption("--browser")
+
+
